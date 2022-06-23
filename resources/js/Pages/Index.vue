8b9ed7048903a29fea14435d86ff3918b0c46794
JB-testdev <jeroenbollen4@hotmail.com>
2022-06-23 11:20:27 +0200

    nav

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
+</script>
+<template>
+<div class="bg-black">
+    <head title="welcome"/>
+    <div class="bg-gradient-to-r from-indigo-500/25 via-purple-500/25 to-pink-500/25 h-24">
+        <header class="text-white flex w-full">
+            <a href="#" class="p-8 text-lg absolute left-0">Jeroen Bollen</a>
+            <nav class="text-lg static p-8 ">
+                <a href="#" class="p-8">Projects</a>
+                <a href="#" class="p-8">Contact</a>
+                <a href="#" class="p-8">About</a>
+            </nav> 
+        </header>
+    </div>
+    <main>
+        lorem ipsum 
+    </main>
+    <footer>
+
+        footer
+    </footer>
+</div>
+
+
+</template>
