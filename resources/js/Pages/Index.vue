bf088ccbf2cb3295c5c14c3252108d670941c7ec
JB-testdev <jeroenbollen4@hotmail.com>
2022-06-28 13:27:04 +0200

    Positioning main content
    Adding grid to main content for a good scalable layout

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -13,7 +13,7 @@ defineProps({
     <head title="welcome"/>
     <div class="bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 width-full">
         <header class="text-white flex w-full">
-            <a href="#" class="pl-8 pt-4 text-lg w-24 absolute">Jeroen Bollen</a>
+            <a href="#" class="pl-56 pt-4 text-lg w-24 absolute">Jeroen Bollen</a>
             <nav class="text-lg  w-full flex justify-center items-center">
                 <div class="p-8"><a href="#" class="link link-underline link-underline-black">Projects</a></div>
                 <div class="p-8"><a href="#" class="link link-underline link-underline-black">Contact</a></div>
@@ -21,26 +21,36 @@ defineProps({
             </nav>
         </header>
     </div>
-    <main >
-   <!-- <div class="p-3 bg-white shadow rounded-lg w-1/2 h-50">
-        <h3 class="text-xs ">font-serif</h3>
-        <p class="font-serif ">
-            The quick brown fox jumps over the lazy dog.
-        </p>
-    </div> -->
+    <main>
+        <div class="grid grid-cols-3 gap-2 place-content-center mt-16 ml-52 mr-52">
+            <div class="p-5 text-white bg-slate-300/25 mr-5 rounded-md text-center place-content-center ">
+                <h1 class="text-2xl font-heading mb-6">    
+                    Terras T'Hoekske
+                </h1>
+                <p >
+                    It has become a lot more busy since the reopening of the catering industry, and because of this the owner of T'Hoekske wanted to expand his terrace. In doing so he needed to have a permit, so he asked me to draw this in 3D.  This has been laid out for the city and has been approved. 
+                </p>
+            </div>
+            <div class="col-span-2">
+                <img src="/images/render.png" class="object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg abosolute"/>
+            </div>
+            <div>
+                <img src="/images/portal.png" class="object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg abosolute"/>
+            </div>
+        </div>
 
-    <div class="container pt-16 pl-52 pr-52 relative">   
-        <div class="p-5 w-96 text-white bg-slate-300/25 rounded-md text-center">
+   <!-- <div class="container mt-16 ml-52 mr-52 flex">   
+        <div class="p-5 w-1/3 text-white bg-slate-300/25 mr-5 rounded-md text-center place-content-center">
             <h1 class="text-2xl font-heading mb-6">    
                 Terras T'Hoekske
             </h1>
-            <p class="">
+            <p>
                It has become a lot more busy since the reopening of the catering industry, and because of this the owner of T'Hoekske wanted to expand his terrace. In doing so he needed to have a permit, so he asked me to draw this in 3D.  This has been laid out for the city and has been approved. 
             </p>
         </div>
 
-        <img src="/images/render.png" class="w-2/3 object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg mb-3 abosolute"/>
-    </div>
+        <img src="/images/render.png" class="w-2/3 object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg abosolute"/>
+    </div>-->
             
     </main>
     <footer class="pl-3">
