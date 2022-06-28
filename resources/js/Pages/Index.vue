8cc95016e28fdf54db8c3ed6434e22502bee7ed0
JB-testdev <jeroenbollen4@hotmail.com>
2022-06-28 21:01:56 +0200

    added portal and center text

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -12,7 +12,7 @@ defineProps({
 <div class="bg-slate-800">
     <head title="welcome"/>
     <div class="bg-gradient-to-r from-indigo-500/50 via-purple-500/50 to-pink-500/50 width-full">
-        <header class="text-white flex w-full">
+        <header class="text-white w-full">
             <a href="#" class="pl-56 pt-4 text-lg w-24 absolute">Jeroen Bollen</a>
             <nav class="text-lg  w-full flex justify-center items-center">
                 <div class="p-8"><a href="#" class="link link-underline link-underline-black">Projects</a></div>
@@ -22,9 +22,9 @@ defineProps({
         </header>
     </div>
     <main>
-        <div class="grid grid-cols-3 gap-2 place-content-center mt-16 ml-52 mr-52">
-            <div class="p-5 text-white bg-slate-300/25 mr-5 rounded-md text-center place-content-center ">
-                <h1 class="text-2xl font-heading mb-6">    
+        <div class="grid grid-cols-3 gap-5 place-content-center mt-16 ml-52 mr-52">
+            <div class="p-5 text-white bg-slate-300/25 rounded-md text-center place-content-center pt-40">
+                <h1 class="text-2xl font-heading mb-6 pt-2">    
                     Terras T'Hoekske
                 </h1>
                 <p >
@@ -33,6 +33,17 @@ defineProps({
             </div>
             <div class="col-span-2">
                 <img src="/images/render.png" class="object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg abosolute"/>
+            </div>
+            <div>
+                <img src="/images/portal.png" class="object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg abosolute"/>
+            </div>
+            <div class="p-5 text-white bg-slate-300/25 rounded-md  text-center">
+                <h1 class="text-2xl font-heading mb-6 pt-16">    
+                    Portal
+                </h1>
+                <p >
+                    It has become a lot more busy since the reopening of the catering industry, and because of this the owner of T'Hoekske wanted to expand his terrace. In doing so he needed to have a permit, so he asked me to draw this in 3D.  This has been laid out for the city and has been approved. 
+                </p>
             </div>
             <div>
                 <img src="/images/portal.png" class="object-contain rounded hover:opacity-50 duration-300 cursor-pointer shadow-lg abosolute"/>
